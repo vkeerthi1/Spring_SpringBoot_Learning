1e3b2950d9229e1da8b367d8cf4266224ac4d0fa
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2021-10-05 16:11:48 +0530

    Add files via upload

---
+++ SpringBoot_Redis/src/main/java/com/home/repository/UserDaoImpl.java
@@ -0,0 +1,64 @@
+package com.home.repository;
+
+import com.home.model.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDAO {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private static final String KEY = "USER";
+
+    @Override
+    public boolean saveUser(User user) {
+        try {
+            redisTemplate.opsForHash().put(KEY, user.getId().toString(), user);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public List<User> fetchAllUser() {
+        List<User> users;
+        users = redisTemplate.opsForHash().values(KEY);
+        return  users;
+    }
+
+    @Override
+    public User fetchUserById(Long id) {
+        User user;
+        user = (User) redisTemplate.opsForHash().get(KEY,id.toString());
+        return user;
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        try {
+            redisTemplate.opsForHash().delete(KEY,id.toString());
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateUser(Long id, User user) {
+        try {
+            redisTemplate.opsForHash().put(KEY, id, user);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
